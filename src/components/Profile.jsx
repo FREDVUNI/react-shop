55cc122b3e825911da05bcdbd65786b66a67591c
TAATA EARL <fredvuni809@gmail.com>
2022-07-30 23:01:50 +0300

    Created a profile page showing decrypted data

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React,{useContext,useEffect} from 'react'
+import '../App.css'
+import { useNavigate } from 'react-router-dom'
+import UserContext from '../context/UserContext'
+import ProfileCard from './ProfileCard'
 
-const Profile = () => {
+const Profile = () =>{
+    const { isLoggedIn }  = useContext(UserContext)
+    let user = isLoggedIn ? isLoggedIn : ''
+    
+    // console.log(user)
+    let navigate = useNavigate()
+    useEffect(()=>{
+        if(!isLoggedIn){
+            navigate("/", { replace: true });
+        }
+    })
     return (
-        <div>
-            
+        <>
+        <div className="container">
+            <div className="content">
+                <div className="row mtop">   
+                    <div className="product-col4">
+                        <img src={"./images/log.svg"} alt="profile"/>
+                    </div>
+                    <div className="sign-col">
+                        <div className="form-box">
+                            <i className="fas fa-user icon-user"></i>
+                            <h2 className="left-">Profile</h2>
+                            <ul className="-span">
+                                {
+                                    <ProfileCard key={user.email} profile={user}/>
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div> 
         </div>
+        </>
     )
 }
 
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
-const ProfileCard = () => {
+const ProfileCard = ({profile}) => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <li>
+                <strong>Username:</strong>
+                {profile.username}
+            </li>
+            <li>
+                <strong>Email:</strong>
+                {profile.email}
+            </li>
+            <li>
+                <strong>Password:</strong>
+                {profile.password}
+            </li>
+        </>
     )
 }
 
