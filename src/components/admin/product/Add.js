cf5848189c27cffc2ee1ce1e6e01451e6c4fbe24
TAATA EARL <fredvuni809@gmail.com>
2022-08-06 11:31:55 +0300

    Added create products and categories

--- src/components/admin/product/Add.js
+++ src/components/admin/product/Add.js
@@ -5,60 +5,65 @@ import UserContext from '../../../context/UserContext'
 import axios from 'axios'
 import {toast} from 'react-toastify' 
 import {url,setHeaders} from '../../../api'
+
+
 const AddProduct = () =>{
-const { auth }  = useContext(UserContext)
-const [error,setError] = useState("");
-let navigate = useNavigate()
-const [categories,setCategories] = useState([])
+    const { auth }  = useContext(UserContext)
+    const [error,setError] = useState("");
+    let navigate = useNavigate()
+    const [categories,setCategories] = useState([])
 
-if(!auth){
-    toast.success("You're not authorized.",{
-        position: toast.POSITION.BOTTOM_RIGHT
-    })
-    navigate("/", { replace: true });
-}
-
-useEffect(()=>{
-    const getCategries = async () =>{
-        const res = await axios.get(`${url}/categories`)
-        const data = await res.data
-        setCategories(data)
+    if(!auth){
+        toast.success("You're not authorized.",{
+            position: toast.POSITION.BOTTOM_RIGHT
+        })
+        navigate("/", { replace: true });
     }
-    getCategries()
-},[])
 
-const [values,setValues] = useState({
-    product:"",
-    price:"",
-    description:"",
-    image:"",
-    categoryId:"",
-})
+    useEffect(()=>{
+        const getCategries = async () =>{
+            const res = await axios.get(`${url}/categories`)
+            const data = await res.data
+            setCategories(data)
+        }
+        getCategries()
+    },[])
 
-const handleSubmit = (e) =>{
-    setError(false)
-    e.preventDefault();  
-    let data = new FormData();
-    data.append('product', values.product);
-    data.append('price', values.price);
-    data.append('description', values.description);
-    data.append('image', values.image);
-    data.append('categoryId', Number(values.categoryId));
-    console.log(data)
-
-axios.post(`${url}/products`,data,setHeaders())
-    .then((data)=>{
-    toast.success("Product has been created.",{
-        position: toast.POSITION.BOTTOM_RIGHT
-    }) 
-    navigate("/", { replace: true });
-})
-.catch((error)=>{
-    console.log(error.response?.data || `There was an error.`)
-    toast.error(error.response?.data.error,{
-        position: toast.POSITION.BOTTOM_RIGHT
+    const [values,setValues] = useState({
+        product:"",
+        price:"",
+        description:"",
+        image:"",
+        categoryId:"",
     })
-})
+
+    const handleSubmit = (e) =>{
+        setError(false)
+        e.preventDefault();  
+        let data = new FormData();
+        data.append('product', values.product);
+        data.append('price', values.price);
+        data.append('description', values.description);
+        // data.append('image', values.image);
+        data.append('categoryId', Number(values.categoryId));
+
+         if(document.getElementById('img').files[0]){
+            data.append('image',document.getElementById('img').files[0])
+        } 
+
+    axios.post(`${url}/products`,data,setHeaders())
+        .then((data)=>{
+        toast.success("Product has been created.",{
+            position: toast.POSITION.BOTTOM_RIGHT
+        }) 
+        navigate("/admin/products", { replace: true });
+    })
+    .catch((error)=>{
+        console.log(error.response?.data || `There was an error.`)
+        toast.error(error.response?.data.error,{
+            position: toast.POSITION.BOTTOM_RIGHT
+        })
+    })
 
 }
 return (
@@ -85,7 +90,7 @@ return (
                <label htmlFor="description">Description</label>
                <textarea cols="40" rows="10" name="description" placeholder="Enter product description" value={values.description}  onChange={(e)=>{setValues({...values,description:e.target.value})}} required></textarea>
                <label htmlFor="image">Image</label>
-               <input type="file" name="image" value={values.image}  onChange={(e)=>{setValues({...values,image:e.target.files[0]})}} required/>
+               <input type="file" name="image" id="img" required/>
                <button type="submit" className="btn">ADD PRODUCT</button>
             </form>
          </div>
